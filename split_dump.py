8f19efd82d9b3f50a72fdb9e6cd1442298dda602
EDANUR <114906335+EdaNur0@users.noreply.github.com>
2023-04-06 15:02:47 +0300

    Add files via upload

---
+++ split_dump.py
@@ -0,0 +1,32 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--database","-D",help="Name of the database to extract")
+parser.add_argument("--dump","-d",help="mysqldump file")
+parser.add_argument("--outfile","-o",help="Name of the file for output")
+args = parser.parse_args()
+print(f"Extracting {args.database} database from {args.dump}")
+
+try:  # try/except güvenlik için yazılır
+    outfile = open(args.outfile, "a", encoding='utf-8')
+    with open(args.dump, "r",encoding='utf-8') as dump_file:  # with open .... işleminden sonra otomatik dosya kendini kapatıyor.
+       find = False
+       for line in dump_file:
+           if line.startswith("CREATE DATABASE /*!32312 IF NOT EXISTS*/"):
+               print(line);
+
+           if line.startswith(f"CREATE DATABASE /*!32312 IF NOT EXISTS*/") and find == True:
+               find = False
+               print(f"CREATE DATABASE /*!32312 IF NOT EXISTS*/ `{args.database}`");
+
+           if line.startswith(f"CREATE DATABASE /*!32312 IF NOT EXISTS*/ `{args.database}`"):
+               find = True
+               print(f"CREATE DATABASE /*!32312 IF NOT EXISTS*/ `{args.database}`");
+
+           if find == True:
+               outfile.write(line)
+    outfile.close()
+
+
+except FileNotFoundError:
+    print("Dump dosyası boştur")
